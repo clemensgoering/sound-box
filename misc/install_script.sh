3626c3849983239d913f5a91754225f894c8ac4c
clemensgoering <clemens.goe@gmx.de>
2025-01-21 21:46:39 +0100

    Updated Installation

--- misc/install_script.sh
+++ misc/install_script.sh
@@ -6,31 +6,65 @@ GIT_BRANCH=${GIT_BRANCH:-main}
 GIT_URL=${GIT_URL:-https://github.com/clemensgoering/sound-box.git}
 DATETIME=$(date +"%Y%m%d_%H%M%S")
 
+SOUNDBOX_HOME_DIR="${HOME_DIR}/Sound-Box"
 
 welcome() {
     clear
     echo "#####################################################
-# You are turning your Raspberry Pi into a Soundbox. Continue with the installation.
-# "
+# You are turning your Raspberry Pi into a Soundbox. 
+# Continue with the installation.
+#####################################################"
     read -rp "Continue interactive installation? [Y/n] " response
     case "$response" in
         [nN][oO]|[nN])
-            exit
+            install "${SOUNDBOX_HOME_DIR}"
             ;;
         *)
-            echo "Installation continues..."
+            echo "Installation cancelled"
             ;;
     esac
 }
 
 finished() {
-    echo "
-#
+    echo "#####################################################
 # INSTALLATION FINISHED
-#
 #####################################################
 
 Let the sounds begin."
+}
+
+create_config_file() {
+    #####################################################
+    # CONFIG FILE
+
+    # Remove existing config file
+    rm "${HOME_DIR}/Configuration.conf"
+    # Create empty config file
+    touch "${HOME_DIR}/Configuration.conf"
+    echo "# Phoniebox config" > "${HOME_DIR}/Configuration.conf"
+}
+
+
+install(){
+    local home_dir="$1"
+    local apt_get="sudo apt-get -qq --yes"
+    local allow_downgrades="--allow-downgrades --allow-remove-essential --allow-change-held-packages"
+    local pip_install="sudo python3 -m pip install --upgrade --force-reinstall -q"
+    local pip_uninstall="sudo python3 -m pip uninstall -y -q"
+    
+    #clear console
+    clear
+
+    create_config_file()
+    
+    # Prepare installation by updating root
+    ${apt_get} update
+    ${apt_get} upgrade
+
+    # Get github code. git must be installed before, even if defined in packages.txt!
+    ${apt_get} install git
+    cd "${HOME_DIR}"
+    git clone ${GIT_URL} --branch "${GIT_BRANCH}"
 }
 
 

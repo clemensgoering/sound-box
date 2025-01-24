0596c988c8c132dba1a5b6d03122fa5c1e458989
clemensgoering <clemens.goe@gmx.de>
2025-01-24 22:03:36 +0100

    Installation Update

--- misc/scripts/install/main.sh
+++ misc/scripts/install/main.sh
@@ -114,8 +114,24 @@ processing_docker(){
 }
 
 loading_git(){    
+    local apt_get="sudo apt-get -qq --yes"
     if [[ ${CONTINUE} == "true" ]]; then
-        bash "${SOUNDBOX_HOME_DIR}/${GIT_REPO}/misc/scripts/install/git.sh"
+        clear
+        echo "
+       _ _     ___           _        _ _ 
+  __ _(_) |_  |_ _|_ __  ___| |_ __ _| | |
+ / _. | | __|  | || '_ \/ __| __/ _. | | |
+| (_| | | |_   | || | | \__ \ || (_| | | |
+ \__, |_|\__| |___|_| |_|___/\__\__,_|_|_|
+ |___/"
+# Get github code. git must be installed before, even if defined in packages.txt!
+        echo ""
+        echo "-- Checking and preparing git init..."
+        ${apt_get} install git
+        echo "-- Create folder and config file"
+        cd "${SOUNDBOX_HOME_DIR}"
+        git clone ${GIT_URL} --branch "${GIT_BRANCH}"
+        echo "-- Fetching git data completed"
         check_continue "Loading NodeJS Data and Dependencies..."
     fi
 }

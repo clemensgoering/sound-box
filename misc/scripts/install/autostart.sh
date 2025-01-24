2a356e768960ddf3da4c2f6c5e63372d095fd0a2
clemensgoering <clemens.goe@gmx.de>
2025-01-24 23:12:20 +0100

    Autostart

--- misc/scripts/install/autostart.sh
+++ misc/scripts/install/autostart.sh
@@ -6,14 +6,21 @@ HOME_DIR=$(getent passwd "$CURRENT_USER" | cut -d: -f6)
 SOUNDBOX_HOME_DIR="${HOME_DIR}/Sound-Box"
 
 autostart(){
-    sudo cp "${SOUNDBOX_HOME_DIR}"/"${GIT_REPO}"/misc/scripts/install/files/soundbox-autostart.service /etc/systemd/system/soundbox-autostart.service
+    local systemd_dir="/etc/systemd/system"
+    # remove and disable old services
+    echo "Remove and disable old services..."
+    sudo systemctl disable soundbox-autostart
+    sudo rm "${systemd_dir}"/rfid-reader.service
+    echo "Copy scripts to system location..."
+    sudo cp "${SOUNDBOX_HOME_DIR}"/"${GIT_REPO}"/misc/scripts/install/files/soundbox-autostart.service "${systemd_dir}"/soundbox-autostart.service
     sudo cp "${SOUNDBOX_HOME_DIR}"/"${GIT_REPO}"/misc/scripts/install/files/soundbox-autostart.sh /usr/local/bin/soundbox-autostart.sh
+    echo "Auth changes required..."
     sudo chmod 744 /usr/local/bin/soundbox-autostart.sh
-    sudo chmod 664 /etc/systemd/system/soundbox-autostart.service
-    sudo systemctl daemon-reload
-    sudo systemctl /etc/systemd/system/soundbox-autostart.service
+    sudo chmod 664 "${systemd_dir}"/soundbox-autostart.service
     sudo chown root:root /usr/local/bin/soundbox-autostart.sh
     # enable the services needed
+    echo "Enable services ..."
+    sudo systemctl daemon-reload
     sudo systemctl enable soundbox-autostart
 
     echo ""

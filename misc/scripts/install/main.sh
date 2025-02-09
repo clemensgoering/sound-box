d5e4d5883ba332e7d9e203e16b5fa06dc981c92e
clemensgoering <clemens.goe@gmx.de>
2025-02-09 21:56:35 +0100

    Git Load Process

--- misc/scripts/install/main.sh
+++ misc/scripts/install/main.sh
@@ -121,8 +121,32 @@ processing_docker(){
 }
 
 loading_git(){    
+    # cant use external bash file
+    # since it first needs to be loaded from the git actually
+    local apt_get="sudo apt-get -qq --yes"
     if [[ ${CONTINUE} == "true" ]]; then
-        bash "${SOUNDBOX_HOME_DIR}/${GIT_REPO}/misc/scripts/install/git.sh"
+        clear
+        echo "
+       _ _     ___           _        _ _ 
+  __ _(_) |_  |_ _|_ __  ___| |_ __ _| | |
+ / _. | | __|  | || '_ \/ __| __/ _. | | |
+| (_| | | |_   | || | | \__ \ || (_| | | |
+ \__, |_|\__| |___|_| |_|___/\__\__,_|_|_|
+ |___/"
+# Get github code. git must be installed before, even if defined in packages.txt!
+        echo ""
+        echo "-- Checking and preparing git init..."
+        ${apt_get} install git
+        echo "-- Create folder and config file"
+        # remove all previous existing folders and files
+        sudo rm -rf "${SOUNDBOX_HOME_DIR}/${GIT_REPO}"
+        
+        # load into temp folder
+        cd "${SOUNDBOX_HOME_DIR}/temp"
+        git clone ${GIT_URL} --branch "${GIT_BRANCH}" && git mv "${SOUNDBOX_HOME_DIR}/temp" "${SOUNDBOX_HOME_DIR}" && rm -rf "${SOUNDBOX_HOME_DIR}/temp"
+        cd "${SOUNDBOX_HOME_DIR}"
+
+        echo "-- Fetching git data completed"
         check_continue "Loading NodeJS Data and Dependencies..."
     fi
 }

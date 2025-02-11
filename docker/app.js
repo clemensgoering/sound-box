443f33357e4a1a565b4cec5a84deaf6a129f0bad
clemensgoering <clemens.goe@gmx.de>
2025-02-11 21:27:06 +0100

    Updated Nodejs Application

--- docker/app.js
+++ docker/app.js
@@ -15,6 +15,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/playlist', playlistRoute);
+app.use('/playlist', playlistRoute);
 
 module.exports = app;
--- docker/routes/index.js
+++ docker/routes/index.js
@@ -2,30 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Soundbox' });
 });
 
-router.get('/overview', async (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'overview.html'));
+router.get('/overview', function(req, res, next) {
+  res.render('overview', { title: 'Overview' });
 });
 
-router.get('/delete', async (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'delete.html'));
+router.get('/add', function(req, res, next) {
+  res.render('add', { title: 'Add' });
 });
 
-router.get('/add', async (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'add.html'));
-});
-
-router.get('/api/rfid', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM playlist');
-    res.json(result.rows);
-  } catch (err) {
-    console.error('Fehler beim Abrufen der RFID-Daten:', err);
-    res.status(500).json({ error: 'Fehler beim Abrufen der RFID-Daten' });
-  }
+router.get('/delete', function(req, res, next) {
+  res.render('delete', { title: 'Delete' });
 });
 
 module.exports = router;
